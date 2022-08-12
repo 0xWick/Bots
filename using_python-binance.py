00699972abc0c5c20dbab8c057d3e48fbb6f5bd6
Polymath <69587947+0xWick@users.noreply.github.com>
2022-08-12 16:33:52 +0500

    Update using_python-binance.py

--- using_python-binance.py
+++ using_python-binance.py
@@ -1,8 +1,8 @@
 from binance.client import Client
 
 
-API_KEY = "iq7rxbTcwRp2bQfPLSN7dkIupwT7Z7767gqAOR6kv6Gl8eM2r8ZNAl3jn1zNNpFC"
-API_SECRET = "idzEA8ymid452YkRjxSY2HIlntcs5HJ094vQ4KqcxSTknHe4q6WSlmgbeg3Sae9i"
+API_KEY = YOUR_API_KEY
+API_SECRET = API_SECRET
 
 def main():
 	client = Client(API_KEY, API_SECRET, testnet=True)
